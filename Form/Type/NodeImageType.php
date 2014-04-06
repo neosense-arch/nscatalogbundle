4d403915f96bd985c72e6114a717047cd0f6a904
Alexey Gromov <italianwind@gmail.com>
2014-04-06 13:30:57 +0400

    NEW Added ns_catalog_node_image form type

---
+++ Form/Type/NodeImageType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace NS\CatalogBundle\Form\Type;
+
+use Doctrine\ORM\EntityRepository;
+use NS\CatalogBundle\Entity\CategoryRepository;
+use NS\CatalogBundle\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToStringTransformer;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Class NodeImageType
+ *
+ * @package NS\CatalogBundle\Form\Type
+ */
+class NodeImageType extends AbstractType
+{
+	/**
+	 * @param OptionsResolverInterface $resolver
+	 */
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'multiple' => false,
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'ns_multi_image';
+    }
+
+    /**
+	 * @return string
+	 */
+	public function getName()
+    {
+        return 'ns_catalog_node_image';
+    }
+}
